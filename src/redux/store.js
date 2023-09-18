f4a0ee07bc4c998ee84694b490fbfe1580119e34
nhule90 <lehoanhu90@gmail.com>
2023-09-17 20:09:50 -0500

    update

---
+++ src/redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import potentialCountriesReducer from "../redux/slices/potentialCountriesSlice";
+import displayedCountryReducer from "./slices/displayCountrySlice";
+export default configureStore({
+    reducer: {
+        potentialCountries: potentialCountriesReducer,
+        displayedCountry: displayedCountryReducer,
+    },
+});
